7cdc59bf58bfafdb6e9788e12da29a10a66152bb
Simplerules14 <47674306+Simplerules14@users.noreply.github.com>
2021-04-25 14:37:33 +0300

    Update pyngpong .py
    updated the code

--- pyngpong .py
+++ pyngpong .py
@@ -5,36 +5,51 @@ win_height = 500
 window = display.set_mode((win_width,win_height))
 display.set_caption("PyngPong")
 
+bg = transform.scale(image.load("background.jpg"),(win_width,win_height))
+
 FPS = 60
 clock = time.Clock()
 game = True
 
 class GameSprite(sprite.Sprite):
-    def __init__ (self, imagge,x,y,size_x,size_y,speed):
+    def __init__ (self, imagge,x,y,size,speed,xsp,ysp):
         super().__init__()
-        self.image = transform.scale(image.load(imagge),(size_x,size_y))
+        self.image = transform.scale(image.load(imagge),(size,size))
         self.speed = speed
         self.rect = self.image.get_rect()
-        self.size_x = size_x
-        self.size_y = size_y
+        self.size = size
         self.rect.x = x
         self.rect.y = y
+        self.xsp = xsp
+        self.ysp = ysp
     def redraw(self):
         window.blit(self.image,(self.rect.x,self.rect.y))
-
+    
 class Ball(GameSprite):
     def update(self):
-        self.rect.x += self.speed
+        if self.rect.y > win_height - self.size:
+            self.ysp = -1
+        if self.rect.y < 0:
+            self.ysp = 1
+        if self.rect.x > win_width - self.size:
+            self.xsp = -1
+        if self.rect.x < 0 :
+            self.xsp = 1
+        self.rect.x += self.speed * self.xsp
+        self.rect.y += self.speed * self.ysp
 
-ball = Ball("Ball.png",300,300,50,50,10)
+        
+
+ball = Ball("Ball.png",300,300,50,2,1,1)
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    window.blit(bg,(0,0))
 
-        ball.update()
-        ball.redraw()
+    ball.update()
+    ball.redraw()
 
-        display.update()
+    display.update()
     clock.tick(FPS)
